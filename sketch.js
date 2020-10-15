caead3ba1f4111513ccaa22a5ead838bf0d7f51d
Sameer <heera.sameer13@gmail.com>
2020-10-15 15:59:18 +0530

    battleship

--- sketch.js
+++ sketch.js
@@ -1,117 +1,94 @@
 const Engine = Matter.Engine;
-const World= Matter.World;
+const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-
+var battleShip, battleShipIMG;
+var alienShip, alienShipImg;
+var pearl, pearlIMG;
+var missiles, missilesImg;
+var alienMissiles, alienMissilesIMG;
 var engine, world;
-var box1, pig1,pig3;
-var backgroundImg,platform;
-var bird, slingshot;
-
-var gameState = "onSling";
-var bg = "sprites/bg1.png";
-var score = 0;
 
 function preload() {
-    getBackgroundImg();
+  battleShipIMG = loadImage("images/battleship.jpg");
+  pearlIMG = loadImage("images/pearl.png");
+  missilesImg = loadImage("images/battleShipMissiles1.png");
+  alienMissilesIMG = loadImage("images/alienmissiles1.png");
 }
 
-function setup(){
-    var canvas = createCanvas(1200,400);
-    engine = Engine.create();
-    world = engine.world;
+function setup() {
+  var canvas = createCanvas(1200, 600);
+  engine = Engine.create();
+  world = engine.world;
+  var options = {
+    isStatic: true
+  }
+  battleShip = Bodies.rectangle(700, 200, 162, 311, options);
+  pearl = Bodies.circle(700, 300, 10, options)
+
+  alienMissiles = Bodies.rectangle(1000, 100, 100, 100, options)
+  World.add(world, battleShip);
+  World.add(world, pearl);
+
+  World.add(world, alienMissiles);
+  console.log(battleShip);
+
+  alienShip1 = new AlienShip(200, 400);
+  alienShip2 = new AlienShip(500, 300);
+  alienShip3 = new AlienShip(900, 100);
+  alienShip4 = new AlienShip(800, 500);
+  alienShip5 = new AlienShip(1000, 300);
 
 
-    ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 170);
 
-    box1 = new Box(700,320,70,70);
-    box2 = new Box(920,320,70,70);
-    pig1 = new Pig(810, 350);
-    log1 = new Log(810,260,300, PI/2);
-
-    box3 = new Box(700,240,70,70);
-    box4 = new Box(920,240,70,70);
-    pig3 = new Pig(810, 220);
-
-    log3 =  new Log(810,180,300, PI/2);
-
-    box5 = new Box(810,160,70,70);
-    log4 = new Log(760,120,150, PI/7);
-    log5 = new Log(870,120,150, -PI/7);
-
-    bird = new Bird(200,50);
-
-    //log6 = new Log(230,180,80, PI/2);
-    slingshot = new SlingShot(bird.body,{x:200, y:50});
 }
 
-function draw(){
-    if(backgroundImg)
-        background(backgroundImg);
-    
-        noStroke();
-        textSize(35)
-        fill("white")
-        text("Score  " + score, width-300, 50)
-    
-    Engine.update(engine);
-    //strokeWeight(4);
-    box1.display();
-    box2.display();
-    ground.display();
-    pig1.display();
-    pig1.score();
-    log1.display();
+function draw() {
+  background("white");
+  Engine.update(engine);
+  imageMode(CENTER);
+  image(battleShipIMG, battleShip.position.x, battleShip.position.y, 100, 100);
+  image(pearlIMG, pearl.position.x, pearl.position.y, 50, 50);
 
-    box3.display();
-    box4.display();
-    pig3.display();
-    pig3.score();
-    log3.display();
+  image(alienMissilesIMG, alienMissiles.position.x, alienMissiles.position.y, 50, 50);
+  alienShip1.display();
+  alienShip2.display();
+  alienShip3.display();
+  alienShip4.display();
+  alienShip5.display();
+  battleShipmovement();
+  alienShipmovement();
 
-    box5.display();
-    log4.display();
-    log5.display();
-
-    bird.display();
-    platform.display();
-    //log6.display();
-    slingshot.display();    
 }
-
-function mouseDragged(){
-    //if (gameState!=="launched"){
-        Matter.Body.setPosition(bird.body, {x: mouseX , y: mouseY});
-    //}
+function battleShipmovement() {
+  if (keyIsDown(LEFT_ARROW)) {
+    battleShip.position.x = battleShip.position.x - 10
+  }
+  if (keyIsDown(RIGHT_ARROW)) {
+    battleShip.position.x = battleShip.position.x + 10
+  }
 }
-
-
-function mouseReleased(){
-    slingshot.fly();
-    gameState = "launched";
+function alienShipmovement() {
+  //as1 = alienShip1.position
+  //Matter.Body.setVelocity(alienShip1, { x:10,y:0 })  
+  //Body.setVelocity( box, {x: 10, y: -10});
+  //alienShip1.velocity.x=-10;
 }
-
-function keyPressed(){
-    if(keyCode === 32){
-       slingshot.attach(bird.body);
-    }
+function createBattleShipMissile() {
+  var options = {
+    isStatic: true
+  }
+  missiles = Bodies.rectangle(battleShip.position.x, battleShip.position.y, 20, options);
+  World.add(world, missiles);
+  console.log(battleShip.position.x)
+  console.log(missiles)
+ // image(missilesImg, missiles.position.x, missiles.position.y, 10, 10);
+  return missiles
 }
+function keyPressed() { 
+  if (keyCode === 66) { 
+    bms= createBattleShipMissile()
+    console.log(bms);
+    Matter.Body.setVelocity(bms, { x: 0, y: -10 });
+ } }
 
-async function getBackgroundImg(){
-    var response = await fetch("http://worldtimeapi.org/api/timezone/Asia/Kolkata");
-    var responseJSON = await response.json();
-
-    var datetime = responseJSON.datetime;
-    var hour = datetime.slice(11,13);
-    
-    if(hour>=0600 && hour<=1900){
-        bg = "sprites/bg1.png";
-    }
-    else{
-        bg = "sprites/bg2.jpg";
-    }
-
-    backgroundImg = loadImage(bg);
-    console.log(backgroundImg);
-}
